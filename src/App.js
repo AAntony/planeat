acb52c837cb0c545eb948de0f6365eefa83c1205
Antony <aldana.antony@gmail.com>
2020-10-22 18:50:59 +0200

    Connexion form OK
    ChefName, go to Wall page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Wall from './components/Wall'
 
 import withFirebase from './hoc/withFirebase'
 
-const App = ({ match }) => {
+const App = ({ match, uid }) => {
   return (
     <Wall
       chefName={match.params.chefName}
+      uid={uid}
     />
   )
 }
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,41 +1,12 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 
-import LoginFB from './LoginFB'
-
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import base, { firebaseApp } from '../base'
-
-import withFirebase from '../hoc/withFirebase'
-import withPlaceholder from '../hoc/withPlaceholder'
+import ConnexionForm from './ConnexionForm'
 
 class Connexion extends React.Component {
   state = {
-    uid: '',
     chefName: '',
     goToApp: false
-  }
-
-  // Si l'utilisateur a dejà un compte
-  // componentDidMount() {
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     if(user) {
-  //       this.handleAuth({ user })
-  //     }
-  //   })
-  // }
-
-  handleAuth = async authData => {
-    this.setState({
-      uid: authData.user.uid,
-      goToApp: true
-    })
-  }
-
-  handleChange = event => {
-    const chefName = event.target.value
-    this.setState({ chefName })
   }
 
   goToApp = event => {
@@ -43,41 +14,23 @@ class Connexion extends React.Component {
     this.setState({ goToApp: true })
   }
 
-  handleAuthenticate = () => {
-    const authProvider = new firebase.auth.FacebookAuthProvider()
-    firebaseApp
-      .auth()
-      .signInWithPopup(authProvider)
-      .then(this.handleAuth)
+  handleChange = event => {
+    const chefName = event.target.value
+    this.setState({ chefName })
   }
 
   render () {
     if (this.state.goToApp) {
-      return <Redirect push to={`/chef/${this.state.chefName}`} />
+      return (<Redirect push to={{ pathname: `/chef/${this.state.chefName}` }} />)
     }
 
     return (
-      <div className='connexionBox'>
-        <form className='connexion' onSubmit={this.goToApp} >
-          <h1>Ma Boîte à Recettes</h1>
-          <input
-            type='text'
-            value={this.state.chefName}
-            onChange={this.handleChange}
-            placeholder={this.props.placeholder}
-            pattern='[A-Za-z-]{1,}'
-            required />
-          <div className='login'>
-            <h2>Connecte toi pour créer tes recettes !</h2>
-            <button onClick={this.handleAuthenticate} className='facebook-button'>Se connecter avec Facebook</button>
-          </div>
-          <p>Pas de caractères spéciaux.</p>
-        </form>
-      </div>
+      <ConnexionForm
+        goToApp= {this.goToApp}
+        chefName= {this.state.chefName}
+        handleChange= {this.handleChange} />
     )
   }
 }
 
-const WrappedComponent = withPlaceholder(Connexion)
-
-export default WrappedComponent
+export default Connexion
